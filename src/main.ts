ef7ee70c0e7d728786e1fcde7fc8b49779588d15
chenxuan <523764720@qq.com>
2019-12-03 15:15:21 +0800

    初步的命令行交互

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,13 @@
-console.log(111);
+import { createInterface } from "readline";
+
+
+let rl = createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.on("line", (input: string) =>
+{
+    console.log(`recv: ${input}`);
+    
+})
